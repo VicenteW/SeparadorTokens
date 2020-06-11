06e3d425260305d52ce4906285b55c1a5b41ee60
VicenteW <35211906+VicenteW@users.noreply.github.com>
2020-06-11 13:54:03 -0500

    Separador de Tokens 3.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SeparadorTokens3._0
+{
+    partial class btnImpLex
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.codigotxt = new System.Windows.Forms.RichTextBox();
+            this.dgvTokens = new System.Windows.Forms.DataGridView();
+            this.Token = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripción = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnLexemas = new System.Windows.Forms.Button();
+            this.lexemastxt = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTokens)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // codigotxt
+            // 
+            this.codigotxt.Location = new System.Drawing.Point(12, 12);
+            this.codigotxt.Name = "codigotxt";
+            this.codigotxt.Size = new System.Drawing.Size(250, 403);
+            this.codigotxt.TabIndex = 0;
+            this.codigotxt.Text = "";
+            // 
+            // dgvTokens
+            // 
+            this.dgvTokens.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTokens.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Token,
+            this.Descripción});
+            this.dgvTokens.Location = new System.Drawing.Point(286, 12);
+            this.dgvTokens.Name = "dgvTokens";
+            this.dgvTokens.RowTemplate.Height = 24;
+            this.dgvTokens.Size = new System.Drawing.Size(453, 447);
+            this.dgvTokens.TabIndex = 1;
+            this.dgvTokens.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTokens_CellContentClick);
+            // 
+            // Token
+            // 
+            this.Token.HeaderText = "Token";
+            this.Token.Name = "Token";
+            // 
+            // Descripción
+            // 
+            this.Descripción.HeaderText = "Descripción";
+            this.Descripción.Name = "Descripción";
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.Location = new System.Drawing.Point(31, 421);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(95, 38);
+            this.btnAnalizar.TabIndex = 2;
+            this.btnAnalizar.Text = "Analizar";
+            this.btnAnalizar.UseVisualStyleBackColor = true;
+            this.btnAnalizar.Click += new System.EventHandler(this.btnAnalizar_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(155, 421);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(95, 38);
+            this.btnImport.TabIndex = 3;
+            this.btnImport.Text = "Importar";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnLexemas
+            // 
+            this.btnLexemas.Location = new System.Drawing.Point(756, 421);
+            this.btnLexemas.Name = "btnLexemas";
+            this.btnLexemas.Size = new System.Drawing.Size(137, 38);
+            this.btnLexemas.TabIndex = 4;
+            this.btnLexemas.Text = "Importar Lexemas";
+            this.btnLexemas.UseVisualStyleBackColor = true;
+            this.btnLexemas.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lexemastxt
+            // 
+            this.lexemastxt.Location = new System.Drawing.Point(756, 12);
+            this.lexemastxt.Name = "lexemastxt";
+            this.lexemastxt.Size = new System.Drawing.Size(254, 403);
+            this.lexemastxt.TabIndex = 5;
+            this.lexemastxt.Text = "";
+            // 
+            // btnImpLex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 471);
+            this.Controls.Add(this.lexemastxt);
+            this.Controls.Add(this.btnLexemas);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnAnalizar);
+            this.Controls.Add(this.dgvTokens);
+            this.Controls.Add(this.codigotxt);
+            this.Name = "btnImpLex";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTokens)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox codigotxt;
+        private System.Windows.Forms.DataGridView dgvTokens;
+        private System.Windows.Forms.Button btnAnalizar;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnLexemas;
+        private System.Windows.Forms.RichTextBox lexemastxt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Token;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripción;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,496 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+
+namespace SeparadorTokens3._0
+{
+    public partial class btnImpLex : Form
+    {
+        public btnImpLex()
+        {
+            InitializeComponent();
+        }
+        public static string[,] lexemas;
+        bool tokens = false;
+        private void btnAnalizar_Click(object sender, EventArgs e)
+        {
+            if (tokens == true) { 
+            string palabra = null;
+            string sig;
+            int parentesis = 0;
+            int llaves = 0;
+            int corchete = 0;
+            bool errorcorchete = false;
+            bool Parentesiserror = false;
+            bool comillas = false;
+            bool migual = false;
+            bool llave = false;
+            bool error = false;
+            bool errorllave = false;
+            Queue encontradas = new Queue();
+            //Comienza el analisis linea por linea
+            for (int j = 0; j < codigotxt.Lines.Count(); j++)
+            {
+                string lineacodigo = codigotxt.Lines[j];
+                char[] separada = lineacodigo.ToCharArray();
+                for (int i = 0; i < separada.Length; i++)
+                {
+                    switch (separada[i])
+                    {
+                        case ' ':
+                            if (comillas == false)
+                            {
+                                if (palabra != null)
+                                {
+                                    BuscarDescripcion(palabra);
+                                    palabra = null;
+                                }
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+                            break;
+
+                        case '+':
+                        case '-':
+                        case '*':
+                        case '/':
+                            if (comillas == false)
+                            {
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                                palabra = palabra + separada[i];
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+                            break;
+
+                        case '=':
+                            if (comillas == false)
+                            {
+                                if (migual == true)
+                                {
+                                    palabra = palabra + separada[i];
+                                    BuscarDescripcion(palabra);
+                                    palabra = null;
+                                }
+                                else
+                                {
+                                    BuscarDescripcion(palabra);
+                                    palabra = null;
+                                    palabra = palabra + separada[i];
+                                    BuscarDescripcion(palabra);
+                                    palabra = null;
+                                }
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+
+                            break;
+
+                        case '<':
+                            if (comillas == false)
+                            {
+                                migual = true;
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                                palabra = palabra + separada[i];
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+                            break;
+
+                        case '>':
+                            if (comillas == false)
+                            {
+                                migual = true;
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                                palabra = palabra + separada[i];
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+                            break;
+
+                        case '.':
+                            if (comillas == false)
+                            {
+                                if (separada[i - 1] == '1' || separada[i - 1] == '2' || separada[i - 1] == '3' || separada[i - 1] == '4' || separada[i - 1] == '5' || separada[i - 1] == '6' || separada[i - 1] == '7' || separada[i - 1] == '8' || separada[i - 1] == '9' || separada[i - 1] == '0')
+                                {
+                                    palabra = palabra + separada[i];
+                                }
+                                else
+                                {
+                                    BuscarDescripcion(palabra);
+                                    palabra = null;
+                                }
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+                            break;
+
+                        case '(':
+                            if (comillas == false)
+                            {
+                                if (palabra != null)
+                                {
+                                    BuscarDescripcion(palabra);
+                                    palabra = null;
+                                }
+                                parentesis++;
+                                palabra = palabra + separada[i];
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+                            break;
+
+                        case ')':
+                            if (comillas == false)
+                            {
+                                if (parentesis != 0)
+                                {
+                                    if (palabra != null)
+                                    {
+                                        BuscarDescripcion(palabra);
+                                        palabra = null;
+                                    }
+
+                                }
+                                else
+                                {
+                                    Parentesiserror = true;
+                                    if (palabra != null)
+                                    {
+                                        BuscarDescripcion(palabra);
+                                        palabra = null;
+                                    }
+                                }
+                                parentesis--;
+                                palabra = palabra + separada[i];
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+
+
+                            break;
+
+                        case '[':
+                            if (comillas == false)
+                            {
+                                corchete++;
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                                palabra = palabra + separada[i];
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+                            break;
+
+                        case ']':
+                            if (comillas == false)
+                            {
+                                corchete--;
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                                palabra = palabra + separada[i];
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+                            break;
+
+                        case '"':
+                            if (comillas == false)
+                            {
+                                comillas = true;
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                                palabra = palabra + separada[i];
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                            }
+                            else
+                            {
+                                if (comillas == true)
+                                {
+                                    comillas = false;
+                                    if (palabra != null)
+                                    {
+                                        BuscarDescripcion(palabra);
+                                        palabra = null;
+                                    }
+                                    palabra = palabra + separada[i];
+                                    BuscarDescripcion(palabra);
+                                    palabra = null;
+                                }
+                            }
+                            break;
+                        case '{':
+                            if (comillas == false)
+                            {
+                                if (llave == false)
+                                {
+                                    llave = true;
+                                }
+                                llaves++;
+                                if (palabra != null)
+                                {
+                                    BuscarDescripcion(palabra);
+                                    palabra = null;
+                                }
+                                palabra = palabra + separada[i];
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+                            break;
+                        case '}':
+                            if (comillas == false)
+                            {
+                                if (llave == true)
+                                {
+                                    llave = false;
+                                }
+                                else
+                                {
+                                    errorllave = true;
+                                }
+                                llaves--;
+                                if (palabra != null)
+                                {
+                                    BuscarDescripcion(palabra);
+                                    palabra = null;
+                                }
+                                palabra = palabra + separada[i];
+                                BuscarDescripcion(palabra);
+                                palabra = null;
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+                            break;
+                        case ';':
+                            if (comillas == false)
+                            {
+                                if (i + 1 != separada.Length)
+                                {
+
+                                    if (comillas != true)
+                                    {
+                                        error = true;
+                                    }
+                                }
+                                else
+                                {
+                                    if (palabra != null)
+                                    {
+                                        BuscarDescripcion(palabra);
+                                        palabra = null;
+                                    }
+                                    if (parentesis != 0)
+                                    {
+                                        Parentesiserror = true;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                palabra = palabra + separada[i];
+                            }
+
+                            break;
+                        default:
+                            palabra = palabra + separada[i];
+                            break;
+                    }
+                    //Termina el switch analizador+
+
+                    if (i + 1 == separada.Length)
+                    {
+                        if (palabra != null)
+                        {
+                            BuscarDescripcion(palabra);
+                            palabra = null;
+                        }
+                        if (error == true)
+                        {
+                            //IMPRIMIR ERROR DE PUNTO Y COMA EN MEDIO
+                        }
+                        if (separada[i] != ';')
+                        {
+                            if (separada[i] == '}')
+                            {
+                            }
+                            else
+                            {
+                                //IMPRIMIR ERROR DE PUNTO Y COMA
+                            }
+                        }
+
+                    }
+                }//termina el for del renglon
+
+
+            }//Termina el for del texto
+        }//if de tokens
+            else
+            {
+                MessageBox.Show("DEBE IMPORTAR PRIMERO EL ARCHIVO DE TOKENS");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                codigotxt.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+        }
+        //BUSCAR DESCRIPCIÓN
+        public void BuscarDescripcion(string palabra)
+        {
+            bool encon = false;
+            for (int i=0; i<lexemas.GetLength(1); i++)
+            {
+                if (palabra != null && lexemas[0,i] != null)
+                {
+                    if (palabra.Equals(lexemas[0, i].ToString()))
+                    {
+                        dgvTokens.Rows.Add(palabra, lexemas[1, i]);
+                        encon = true;
+                        break;
+                    }
+                }
+            }
+            if(encon == false)
+            {
+                if (palabra != null)
+                {
+                    char[] variable = palabra.ToCharArray();
+                    bool numerico = true;
+                    for (int i = 0; i < palabra.Length; i++)
+                    {
+                        if (variable[i] != '0' && variable[i] != '1' && variable[i] != '2' && variable[i] != '3' && variable[i] != '4' && variable[i] != '5' && variable[i] != '6' && variable[i] != '7' && variable[i] != '8' && variable[i] != '9')
+                        {
+                            numerico = false;
+                        }
+                        //SI ES UNA VARIABLE
+                    }
+                    if (numerico == true)
+                    {
+                        dgvTokens.Rows.Add(palabra, "Valor numerico");
+                    }
+                    if (numerico == false)
+                    {
+                        dgvTokens.Rows.Add(palabra, "Variable");
+                    }
+
+                }
+            }
+        }//Cierra el metodo de busqueda de descripción
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tokens = true;
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                lexemastxt.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+            lexemas = new string[2, lexemastxt.Lines.Count()];
+            for (int i=0; i<lexemastxt.Lines.Count(); i++)
+            {
+                //string[,] lexemas = new string[2, lexemastxt.Lines.Count()];
+                string lexema = lexemastxt.Lines[i];
+                bool div = false;
+                string palabra = null;
+                char[] arraylexema= lexema.ToCharArray();
+                for(int j=0; j<arraylexema.Length; j++)
+                {
+                    if (arraylexema[j] != '-')
+                    {
+                        
+                        if (div == true)
+                        {
+                            palabra = palabra + arraylexema[j];
+                            
+                        }else
+                        {
+                            palabra = palabra + arraylexema[j];
+                        }
+                    }
+                    else
+                    {
+                        lexemas[0, i] = palabra;
+
+                        div = true;
+                        palabra = null;
+
+                    }
+                    
+                }
+                lexemas[1, i] = palabra;
+
+            }
+        }
+
+        public void Llenargdv()
+        {
+            
+        }
+
+        private void dgvTokens_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
